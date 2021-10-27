f14e81fdf962220300275d7ba81eca58d65eb5bb
Ng Rui Yong <ruiyong@redairship.com>
2021-10-27 22:17:49 +0800

    feat: add image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: `${process.env.NEXT_PUBLIC_BASE_PATH || 'http://localhost:3000'}/`
+  },
 }
